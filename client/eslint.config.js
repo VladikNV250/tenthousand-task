b6383a94844f5f3c34a294cf817350862416f0f8
VladikNV250 <vladiknemichev@gmail.com>
2026-03-19 01:34:37 +0200

    build(TT-12): add no-console rule to eslint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -67,6 +67,9 @@ export default defineConfig([
             'simple-import-sort/imports': 'warn',
             'simple-import-sort/exports': 'warn',
 
+            // --- General ---
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+
             // --- TypeScript ---
             '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
             '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
