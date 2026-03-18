f2c964f9dc143b45c897afd66b9f26b1d6fd3190
VladikNV250 <vladiknemichev@gmail.com>
2026-03-18 20:33:52 +0200

    feat(TT-11): generated graphql queries and mutations

--- client/src/services/__generated__/generated.ts
+++ client/src/services/__generated__/generated.ts
@@ -3,22 +3,113 @@ import * as Types from './graphql-types'
 import { TypedDocumentString } from './graphql-types'
 import { api } from '@/services/baseApi'
 
-export const GetBooksDocument = new TypedDocumentString(`
-    query GetBooks {
-  books {
+export const GetAllFormsDocument = new TypedDocumentString(`
+    query GetAllForms {
+  forms {
+    id
     title
-    author
+    description
+    questions {
+      id
+      text
+      type
+      options
+    }
+  }
+}
+    `)
+export const GetResponsesByFormDocument = new TypedDocumentString(`
+    query GetResponsesByForm($formId: ID!) {
+  responses(formId: $formId) {
+    id
+    answers {
+      questionId
+      value
+    }
+  }
+}
+    `)
+export const GetFormByIdDocument = new TypedDocumentString(`
+    query GetFormByID($id: ID!) {
+  form(id: $id) {
+    title
+    description
+    questions {
+      id
+      text
+      type
+      options
+    }
+  }
+}
+    `)
+export const CreateNewFormDocument = new TypedDocumentString(`
+    mutation CreateNewForm($title: String!, $description: String, $questions: [QuestionInput!]) {
+  createForm(title: $title, description: $description, questions: $questions) {
+    id
+    title
+    description
+    questions {
+      id
+      text
+      type
+      required
+      options
+    }
+  }
+}
+    `)
+export const SubmitFormResponseDocument = new TypedDocumentString(`
+    mutation SubmitFormResponse($formId: ID!, $answers: [AnswerInput!]) {
+  submitResponse(formId: $formId, answers: $answers) {
+    id
+    formId
+    answers {
+      id
+      questionId
+      value
+    }
   }
 }
     `)
 
 const injectedRtkApi = api.injectEndpoints({
     endpoints: (build) => ({
-        GetBooks: build.query<Types.GetBooksQuery, Types.GetBooksQueryVariables | void>({
-            query: (variables) => ({ document: GetBooksDocument, variables }),
+        GetAllForms: build.query<Types.GetAllFormsQuery, Types.GetAllFormsQueryVariables | void>({
+            query: (variables) => ({ document: GetAllFormsDocument, variables }),
+        }),
+        GetResponsesByForm: build.query<
+            Types.GetResponsesByFormQuery,
+            Types.GetResponsesByFormQueryVariables
+        >({
+            query: (variables) => ({ document: GetResponsesByFormDocument, variables }),
+        }),
+        GetFormByID: build.query<Types.GetFormByIdQuery, Types.GetFormByIdQueryVariables>({
+            query: (variables) => ({ document: GetFormByIdDocument, variables }),
+        }),
+        CreateNewForm: build.mutation<
+            Types.CreateNewFormMutation,
+            Types.CreateNewFormMutationVariables
+        >({
+            query: (variables) => ({ document: CreateNewFormDocument, variables }),
+        }),
+        SubmitFormResponse: build.mutation<
+            Types.SubmitFormResponseMutation,
+            Types.SubmitFormResponseMutationVariables
+        >({
+            query: (variables) => ({ document: SubmitFormResponseDocument, variables }),
         }),
     }),
 })
 
 export { injectedRtkApi as api }
-export const { useGetBooksQuery, useLazyGetBooksQuery } = injectedRtkApi
+export const {
+    useGetAllFormsQuery,
+    useLazyGetAllFormsQuery,
+    useGetResponsesByFormQuery,
+    useLazyGetResponsesByFormQuery,
+    useGetFormByIdQuery,
+    useLazyGetFormByIdQuery,
+    useCreateNewFormMutation,
+    useSubmitFormResponseMutation,
+} = injectedRtkApi
--- client/src/services/baseApi.ts
+++ client/src/services/baseApi.ts
@@ -2,9 +2,24 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
 import { GraphQLClient } from 'graphql-request'
 
-export const client = new GraphQLClient('http://localhost:4000/graphql')
+const client = new GraphQLClient('http://localhost:4000/graphql')
+
+const rawBaseQuery = graphqlRequestBaseQuery({ client })
+
+const customBaseQuery: typeof rawBaseQuery = async (args, api, extraOptions) => {
+    let parsedArgs = args
+
+    if (typeof args === 'object' && args.document && args.document instanceof String) {
+        parsedArgs = {
+            ...args,
+            document: (args.document as string).toString(),
+        }
+    }
+
+    return rawBaseQuery(parsedArgs, api, extraOptions)
+}
 
 export const api = createApi({
-    baseQuery: graphqlRequestBaseQuery({ client }),
+    baseQuery: customBaseQuery,
     endpoints: () => ({}),
 })
