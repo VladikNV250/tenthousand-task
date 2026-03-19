22ca82c82b8f575911eb1005304eedeaca8ecc90
VladikNV250 <vladiknemichev@gmail.com>
2026-03-19 02:14:46 +0200

    refactor(TT-12): use skipToken to prevent sending empty id for getting form

--- client/src/hooks/useFormFiller.ts
+++ client/src/hooks/useFormFiller.ts
@@ -1,7 +1,9 @@
+import { skipToken } from '@reduxjs/toolkit/query'
 import type { SubmitEvent } from 'react'
 import { useParams } from 'react-router'
 
 import {
+    type AnswerInput,
     useGetFormByIdQuery,
     useSubmitFormResponseMutation,
 } from '@/services/__generated__/graphql'
@@ -13,7 +15,7 @@ export const useFormFiller = () => {
     const dispatch = useAppDispatch()
     const { id } = useParams()
 
-    const { data, isLoading } = useGetFormByIdQuery({ id: id ?? '' })
+    const { data, isLoading } = useGetFormByIdQuery(id ? { id } : skipToken)
     const formFiller = useAppSelector(selectFormFiller)
 
     const [submitFormResponse, { isLoading: isSubmitting, isSuccess, reset }] =
@@ -32,7 +34,9 @@ export const useFormFiller = () => {
             }
             await submitFormResponse({
                 formId: id,
-                answers: Object.values(formFiller.answers),
+                answers: Object.values(formFiller.answers).filter(
+                    (answer): answer is AnswerInput => answer !== undefined,
+                ),
             }).unwrap()
             dispatch(clearAnswers())
         } catch (e) {
